486bc6bc459f2a969029b79b8cbdcdc981e49376
Nagendra3414 <gadinagendra3414@gmail.com>
2023-05-29 10:38:05 +0530

    Modified few lines in the script

--- src/test/java/pages/DummyClass.java
+++ src/test/java/pages/DummyClass.java
@@ -1,25 +1,26 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class DummyClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
-		System.out.println("New script added");
-		
 		System.out.println("New script added");
 
-		
-		
 		System.out.println("New script added");
 
-		
-		
+		System.out.println("Nag modified script");
+
+		WebDriver driver = new ChromeDriver();
+
 		System.out.println("New script added");
 
-		
 		System.out.println("New script added");
+
+		System.out.println("Nag added new line");
 
 	}
 
