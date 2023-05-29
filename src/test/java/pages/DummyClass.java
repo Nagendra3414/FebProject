18dbc56d8f544b33e26daad2fc5a396a7d2d8e98
Nagendra3414 <gadinagendra3414@gmail.com>
2023-05-29 10:32:29 +0530

    Deppak added new class called DummyClass

---
+++ src/test/java/pages/DummyClass.java
@@ -0,0 +1,26 @@
+package pages;
+
+public class DummyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("New script added");
+		
+		System.out.println("New script added");
+
+		
+		
+		System.out.println("New script added");
+
+		
+		
+		System.out.println("New script added");
+
+		
+		System.out.println("New script added");
+
+	}
+
+}
